d7ab5669224c330972aa77bf8b7736634ad4844a
Woooj <aldongaradal@gmail.com>
2020-02-21 04:53:22 +0600

    3rd commit

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -4,10 +4,13 @@ import com.example.mapper.InfoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
-public class GreetingController {
+@Controller
+public class MainController {
 
     @Autowired
     InfoMapper infoMapper;
@@ -15,9 +18,7 @@ public class GreetingController {
     @RequestMapping(value = "/main", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<String> main() {
-
-//            model.addAttribute("name", String.valueOf(infoMapper.findById(info.getId())));
-            return new ResponseEntity(infoMapper.findById(1), HttpStatus.OK);
+         return new ResponseEntity(infoMapper.findById(1), HttpStatus.OK);
     }
 
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS datatable
 (
-    id INTEGER PRIMARY KEY auto_increment,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     info VARCHAR
 );
+
+INSERT INTO datatable(info) VALUES ('Hello World' );
